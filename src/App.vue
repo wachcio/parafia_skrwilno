c93e390e6d55466f22742f385c783b200733b4f9
Wachcio <wachcio@poczta.fm>
2019-01-18 03:34:27 +0100

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,67 @@
 <template>
   <div id="app">
     <div class="container">
-      <nav></nav>
-      <section class="first"></section>
-      <section class="wordOfGod"></section>
-      <section class="news"></section>
-      <section class="ourChurch"></section>
-      <section class="holyMasses"></section>
-      <section class="clergy"></section>
-      <section class="gallery"></section>
-      <section class="history"></section>
-      <section class="contact"></section>
-      <footer>Wachcio&copy;2019</footer>
+      <Navigation/>
+      <section id="first"></section>
+      <section id="wordOfGod"></section>
+      <section id="news"></section>
+      <section id="ourChurch"></section>
+      <section id="holyMasses"></section>
+      <section id="clergy"></section>
+      <section id="gallery"></section>
+      <section id="history"></section>
+      <section id="contact"></section>
+      <footer>
+        <h1>Wachcio&copy;2019</h1>
+      </footer>
     </div>
   </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation";
 export default {
   name: "app",
-  components: {}
+  components: { Navigation }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Roboto", sans-serif;
+  font-size: 15px;
+  color: #f3bc3f;
+  background-color: whitesmoke;
+  min-width: 250px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+footer {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  height: 60px;
+  background-color: #222;
+  & h1 {
+    width: 100%;
+    font-size: 1em;
+  }
+}
+section {
+  height: 150px;
+  color: #222;
 }
 </style>
